5009c8644f0640af71f12198e640a23a0b2cfc1d
Ken Shaffer <8398167+strobelight@users.noreply.github.com>
2023-04-05 16:56:50 -0400

    local registry ui

--- examples/startK3sMultiDocker.sh
+++ examples/startK3sMultiDocker.sh
@@ -33,5 +33,36 @@ docker context create lima-$INSTANCE_NAME --docker "host=unix://${HOME}/.lima/$I
 docker context use lima-$INSTANCE_NAME
 
 # set up registry
-mkdir -p ~/Docker-Storage
-docker run -d --restart=always -p "127.0.0.1:5000:5000" -v ~/Docker-Storage:/var/lib/registry --name registry registry
+DOCKER_STORAGE=$HOME/Docker-Storage
+mkdir -p $DOCKER_STORAGE
+cat <<EOF > ${DOCKER_STORAGE}/registry_config.yml
+version: 0.1
+log:
+  fields:
+    service: registry
+storage:
+  delete:
+    enabled: true
+  cache:
+    blobdescriptor: inmemory
+  filesystem:
+    rootdirectory: /var/lib/registry
+http:
+  addr: :5000
+  headers:
+    X-Content-Type-Options: [nosniff]
+    Access-Control-Allow-Origin: ['*']
+    Access-Control-Allow-Methods: ['HEAD', 'GET', 'OPTIONS', 'DELETE']
+    Access-Control-Allow-Headers: ['Authorization', 'Accept', 'Cache-Control']
+    Access-Control-Max-Age: [1728000]
+    Access-Control-Expose-Headers: ['Docker-Content-Digest']
+EOF
+docker run -d --restart=always -p "127.0.0.1:5000:5000" \
+    -v ${DOCKER_STORAGE}:/var/lib/registry \
+    -v ${DOCKER_STORAGE}/registry_config.yml:/etc/docker/registry/config.yml \
+    --name registry registry
+
+docker run -d --restart=always -p "8080:80" -e DELETE_IMAGES=true -e REGISTRY_TITLE="Localhost Registry" -e REGISTRY_URL="http://localhost:5000" -e SINGLE_REGISTRY=true --name registry-ui joxit/docker-registry-ui:latest
+
+# After delete, run
+#   docker exec registry registry garbage-collect /etc/docker/registry/config.yml
